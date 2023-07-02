991a729263b8de871b7275fe35d2e430400927bc
Michael <7471919@naver.com>
2023-07-02 16:27:09 +0900

    setting: docker setting

--- backend/fastapi_server/src/api/index.py
+++ backend/fastapi_server/src/api/index.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
 api_router = router = APIRouter()
+
+
+@router.get("/v1/{username}")
+async def getUser(username: str):
+    return {"user": username}
--- backend/nest_server/src/main.ts
+++ backend/nest_server/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './database/prisma/prisma.service';
+import { swaggerSetUp } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
+  // const prismaService = app.get(PrismaService);
+  // await prismaService.enableShutdownHooks(app);
+
+  swaggerSetUp(app);
+
+  app.enableCors();
 
   await app.listen(3000);
 }
